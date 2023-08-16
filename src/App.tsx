0d02c25f49d88473c0403e9048ed0cf85655956b
chinsun9 <chin_sung@naver.com>
2023-08-16 10:58:10 +0900

    example

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,58 @@
+import { useRef } from "react";
 import "./App.css";
 
 function App() {
-  return <>hello</>;
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleClick = () => {
+    if (
+      !("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices)
+    ) {
+      alert("카메라에 접근할 수 없습니다");
+      return;
+    }
+
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true,
+        audio: true,
+      })
+      .then((stream) => {
+        const video = videoRef.current;
+        if (!video) return;
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+          video.play();
+        });
+      })
+      .catch(alert);
+  };
+  return (
+    <>
+      <label>
+        사진 찍기
+        <input type="file" accept="image/*" capture="environment" />
+      </label>
+
+      <label>
+        영상 찍기
+        <input type="file" accept="video/*" capture="environment" />
+      </label>
+
+      <div>
+        <div
+          style={{
+            display: "flex",
+            background: "gray",
+          }}
+        >
+          <video ref={videoRef} muted />
+        </div>
+
+        <button onClick={handleClick}>웹캠</button>
+      </div>
+    </>
+  );
 }
 
 export default App;
